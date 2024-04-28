0a117f1856b254bfd92e490513e8c951d58a307b
Aaron Adrian <aaroncadrian@gmail.com>
2024-04-28 14:18:37 -0700

    Add skeleton for create enrollment

--- apps/svc-ses/src/app/enrollments.controller.ts
+++ apps/svc-ses/src/app/enrollments.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
 
 @Controller()
 export class EnrollmentsController {
+  constructor(private dynamo: DynamoDBClient) {}
 
+  @Post('CreateEnrollment')
+  async CreateEnrollment() {
+    const result = await this.dynamo.send(
+      new PutItemCommand({
+        // TODO: Add table name
+        TableName: '',
+        Item: marshall({
+          pk: '',
+          sk: '',
+          personId: '',
+          createdAt: '',
+        }),
+      })
+    );
+
+    return {
+      enrollment: {
+        id: 'hello-world',
+      },
+    };
+  }
 }
