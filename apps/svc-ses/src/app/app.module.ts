a1e32ce2ebd8af1bfb9909828cc9e2143685d24d
Aaron Adrian <aaroncadrian@gmail.com>
2024-04-28 14:00:35 -0700

    Create enrollments controller

--- apps/svc-ses/src/app/app.module.ts
+++ apps/svc-ses/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnrollmentsController } from './enrollments.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EnrollmentsController],
   providers: [AppService],
 })
 export class AppModule {}
