faa2d05a519c9ce777dd7267cc02cd207ac869ab
Aaron Adrian <aaroncadrian@gmail.com>
2024-04-28 18:26:59 -0700

    Copy enrollments controller

--- apps/svc-ses/src/app/controllers/enrollments.controller.ts
+++ apps/svc-ses/src/app/controllers/enrollments.controller.ts
@@ -1,29 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  DeleteItemCommand,
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  QueryCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { Enrollment } from '../models/enrollment.model';
+import { stripPkSk } from '../utils/strip-pk-sk';
 import {
   ListEnrollmentsCommandInput,
   ListEnrollmentsCommandOutput,
 } from '../commands/list-enrollments.command';
 import {
-  GetEnrollmentCommandInput,
-  GetEnrollmentCommandOutput,
-} from '../commands/get-enrollment.command';
-import { Enrollment } from '../models/enrollment.model';
-import {
   CreateEnrollmentCommandInput,
   CreateEnrollmentCommandOutput,
 } from '../commands/create-enrollment.command';
-import { getCurrentTimestamp } from '../utils/get-current-timestamp';
-import {
-  DynamoDBClient,
-  QueryCommand,
-  PutItemCommand,
-  GetItemCommand,
-  DeleteItemCommand,
-} from '@aws-sdk/client-dynamodb';
 import {
   DeleteEnrollmentCommandInput,
   DeleteEnrollmentCommandOutput,
 } from '../commands/delete-enrollment.command';
+import {
+  GetEnrollmentCommandInput,
+  GetEnrollmentCommandOutput,
+} from '../commands/get-enrollment.command';
 
 @Controller()
 export class EnrollmentsController {
@@ -35,7 +36,22 @@ export class EnrollmentsController {
   ): Promise<ListEnrollmentsCommandOutput> {
     const { rosterId } = input;
 
-    const enrollments = [];
+    const result = await this.dynamo.send(
+      new QueryCommand({
+        TableName: 'local.ses-01',
+        KeyConditionExpression: '#pk = :pk',
+        ExpressionAttributeNames: {
+          '#pk': 'pk',
+        },
+        ExpressionAttributeValues: marshall({
+          ':pk': rosterId,
+        }),
+      })
+    );
+
+    const enrollments: Enrollment[] = result.Items.map(
+      (item) => stripPkSk(unmarshall(item) as any) as Enrollment
+    );
 
     return {
       enrollments,
@@ -48,9 +64,23 @@ export class EnrollmentsController {
   ): Promise<GetEnrollmentCommandOutput> {
     const { rosterId, personId } = input;
 
-    const enrollment = {} as Enrollment;
+    const result = await this.dynamo.send(
+      new GetItemCommand({
+        TableName: 'local.ses-01',
+        Key: marshall({
+          pk: rosterId,
+          sk: personId,
+        }),
+      })
+    );
 
-    // const result = await this.dynamo.send(new QueryCommand({}));
+    const item = result.Item;
+
+    if (!item) {
+      throw new BadRequestException('Enrollment Not Found');
+    }
+
+    const enrollment = stripPkSk(unmarshall(item) as any) as Enrollment;
 
     return {
       enrollment,
@@ -63,7 +93,7 @@ export class EnrollmentsController {
   ): Promise<CreateEnrollmentCommandOutput> {
     const { rosterId, personId } = input;
 
-    const currentTimestamp = getCurrentTimestamp();
+    const currentTimestamp = new Date().toISOString();
 
     const enrollment: Enrollment = {
       rosterId,
@@ -71,9 +101,16 @@ export class EnrollmentsController {
       createdAt: currentTimestamp,
     };
 
-    // await this.dynamo.send(new PutItemCommand({
-    //
-    // }));
+    await this.dynamo.send(
+      new PutItemCommand({
+        TableName: 'local.ses-01',
+        Item: marshall({
+          pk: rosterId,
+          sk: personId,
+          ...enrollment,
+        }),
+      })
+    );
 
     return {
       enrollment,
@@ -86,11 +123,32 @@ export class EnrollmentsController {
   ): Promise<DeleteEnrollmentCommandOutput> {
     const { rosterId, personId } = input;
 
-    // const result = await this.dynamo.send(new DeleteItemCommand({
-    //
-    // }));
+    const result = await this.dynamo
+      .send(
+        new DeleteItemCommand({
+          ReturnValues: 'ALL_OLD',
+          TableName: 'local.ses-01',
+          Key: marshall({
+            pk: rosterId,
+            sk: personId,
+          }),
+          ConditionExpression: 'attribute_exists(#pk)',
+          ExpressionAttributeNames: {
+            '#pk': 'pk',
+          },
+        })
+      )
+      .catch((error) => {
+        if (error.name === 'ConditionalCheckFailedException') {
+          throw new BadRequestException('Enrollment Not Found');
+        }
 
-    const enrollment: Enrollment = {} as Enrollment;
+        throw error;
+      });
+
+    const enrollment = stripPkSk(
+      unmarshall(result.Attributes) as any
+    ) as Enrollment;
 
     return {
       enrollment,
